a01e257cbdd1975ce1f5707c6b98948d07225ffc
Ali Shanks <a.shanks@live.com>
2022-10-05 22:52:36 +0100

    added mirror model to mirrors, adjusted scaling and alignment, added modular interactable script and edited player interact script

--- Tuna Game/Assets/Scripts/Puzzles/Mirror Puzzle/PlayerInteractable.cs
+++ Tuna Game/Assets/Scripts/Puzzles/Mirror Puzzle/PlayerInteractable.cs
@@ -2,13 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurnMirror : MonoBehaviour
+public class PlayerInteractable : MonoBehaviour
 {
-
     //set up a vector 3 containing the number by which to rotate the mirror
     private Vector3 rotationOfMirror = new Vector3(0, 45, 0);
 
+
+    public void PlayerInteract()
+    {
+        Debug.Log("message received");
+
+        if (this.tag == "Mirror") 
+        {
+            TurnThisMirror();
+        }
+    }
     
+
+
+
+
+
     public void TurnThisMirror()
     {
         //add the rotation factor onto the current rotation of the mirror.
